8cbe61c183ed8355e2e08007d5d2178f4d1b1e09
hang.liu <hang.liu@thoughtworks.com>
2021-05-03 12:27:40 +0800

    add switch in layout

--- src/App.js
+++ src/App.js
@@ -16,9 +16,11 @@ function App() {
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
         <BaseLayout>
-          <Route path="/user" component={User} />
-          <Route path="/articles" component={Articles} />
-          <Route path="/" component={Home} />
+          <Switch>
+            <Route path="/user" component={User} />
+            <Route path="/articles" component={Articles} />
+            <Route path="/" component={Home} />
+          </Switch>
         </BaseLayout>
       </Switch>
     </Router>
