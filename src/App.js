8a41ff332abc546b9a7b4f59d602a728075072a7
hang <isixline@qq.com>
2021-07-31 22:11:01 +0800

    feat: add new BaseLayout

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import "./App.css";
 import "antd/dist/antd.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
-import Home from "./components/Home";
 import User from "./components/User";
-import Article from "./components/Article";
-import NewArticle from "./components/NewArticle";
-import Articles from "./components/Articles";
 
 import BaseLayout from "./components/BaseLayout";
+import Question from "./components/Question";
+import Group from "./components/Group";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        <BaseLayout>
-          <Switch>
-            <Route path="/user" component={User} />
-            <Route path="/articles/new" component={NewArticle} />
-            <Route path="/articles/:id" component={Article} />
-            <Route path="/articles" component={Articles} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </BaseLayout>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/register" component={Register}/>
+                <Route path="/login" component={Login}/>
+                <BaseLayout>
+                    <Switch>
+                        <Route path="/user" component={User}/>
+                        <Route path="/questions" component={Question}/>
+                        <Route path="/groups" component={Group}/>
+                        <Route path="/me" component={User}/>
+                    </Switch>
+                </BaseLayout>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,31 +1,29 @@
-import { Link } from "react-router-dom";
-import { Layout, Menu } from "antd";
+import {Link} from "react-router-dom";
+import {Layout, Menu} from "antd";
 import "./BaseLayout.css";
 
-const { Header, Content } = Layout;
+const {Header, Content} = Layout;
 
 export default function BaseLayout(props) {
-  return (
-    <Layout>
-      <Header>
-        <Menu theme="dark" mode="horizontal" selectedKeys={props.location.pathname}>
-          <Menu.Item key="/">
-            <Link to="/">首页</Link>
-          </Menu.Item>
-          <Menu.Item key="/articles">
-            <Link to="/articles">发现</Link>
-          </Menu.Item>
-          <Menu.Item key="/articles/new">
-            <Link to="/articles/new">创作</Link>
-          </Menu.Item>
-          <Menu.Item key="/user">
-            <Link to="/user">个人中心</Link>
-          </Menu.Item>
-        </Menu>
-      </Header>
-      <Content>
-        <div className="site-layout-content">{props.children}</div>
-      </Content>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Header>
+                <span className="logo" >问答平台</span>
+                <Menu theme="dark" mode="horizontal" selectedKeys={props.location.pathname}>
+                    <Menu.Item key="/questions">
+                        <Link to="/questions">问题</Link>
+                    </Menu.Item>
+                    <Menu.Item key="/groups">
+                        <Link to="/groups">圈子</Link>
+                    </Menu.Item>
+                    <Menu.Item key="/me">
+                        <Link to="/me">个人中心</Link>
+                    </Menu.Item>
+                </Menu>
+            </Header>
+            <Content>
+                <div className="site-layout-content">{props.children}</div>
+            </Content>
+        </Layout>
+    );
 }
