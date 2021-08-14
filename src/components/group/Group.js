8cb44aac6a13227a8a7f604306e6ebec9206c221
hang <isixline@qq.com>
2021-08-14 13:15:07 +0800

    feat: join group and exit group

--- src/components/group/Group.js
+++ src/components/group/Group.js
@@ -1,10 +1,14 @@
-import {message} from "antd";
+import {Button, message} from "antd";
 import React, {useEffect, useState} from "react"
 import axios from "../../common/axios";
 import Questions from "../question/Questions";
 
+import "./Group.css";
+
 export default function Group(props) {
     const [groupInfo, setGroupInfo] = useState({});
+    const userId = window.localStorage.userId;
+    const id = props.match.params.id;
 
     function getGroup(id) {
         axios.get(`/groups/${id}`)
@@ -17,15 +21,46 @@ export default function Group(props) {
     }
 
     useEffect(() => {
-        getGroup(props.match.params.id);
+        getGroup(id);
     }, []);
+
+    function joinGroup() {
+        axios
+            .post(`/groups/${id}/members`)
+            .then(function (response) {
+                message.success("加入成功");
+                getGroup(id);
+            })
+            .catch(function (error) {
+                message.error("加入失败");
+            });
+    }
+
+    function exitGroup() {
+        axios
+            .delete(`/groups/${id}/members/me`)
+            .then(function (response) {
+                message.success("退出成功");
+                getGroup(id);
+            })
+            .catch(function (error) {
+                message.error("退出失败");
+            });
+    }
 
     return (
         <div>
-            <h2>{groupInfo.name}</h2>
+            <div className="group-heater">
+                <h2>{groupInfo.name}</h2>
+                {
+                    groupInfo.members?.find(member => member.userId === userId)
+                        ? <Button type="primary" onClick={() => exitGroup()}>退出圈子</Button>
+                        : <Button type="primary" onClick={() => joinGroup()}>加入圈子</Button>
+                }
+            </div>
             <p>创建时间：{groupInfo.createdAt}，创建者：{groupInfo.creator?.name}，共有{groupInfo.members?.length}位成员</p>
-            <p>{groupInfo.description}</p>
-            <Questions groupId={props.match.params.id}></Questions>
+            <p>描述：{groupInfo.description}</p>
+            <Questions groupId={id}></Questions>
         </div>
     );
 }
