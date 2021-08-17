52593bf92eb62c48c97ea40e5ce3fad6054a8d4f
hang <isixline@qq.com>
2021-08-17 22:34:42 +0800

    feat: change group member role

--- src/components/group/GroupMemberManagement.js
+++ src/components/group/GroupMemberManagement.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from "react"
-import {Button, Card, message, Pagination} from "antd";
+import {Button, message, Pagination, Select} from "antd";
 import axios from "../../common/axios";
 
 import "./GroupMemberManagement.css"
+import {Option} from "antd/es/mentions";
 
 export default function GroupMemberManagement(props) {
     const size = 10;
@@ -10,18 +11,13 @@ export default function GroupMemberManagement(props) {
     const [page, setPage] = useState(1);
     const [total, setTotal] = useState(0);
 
-    const roleMap = {
-        "NORMAL": "普通成员",
-        "OWNER": "所有者"
-    }
-
     useEffect(() => {
         getGroups(page, size);
     }, [page]);
 
     function getGroups(page, size) {
         axios
-            .get(`/groups/${props.groupId}/members`, {
+            .get(`/groups/${props.groupId}/members/management`, {
                 params: {
                     page: page - 1,
                     size,
@@ -41,13 +37,30 @@ export default function GroupMemberManagement(props) {
         setPage(page);
     }
 
+    function handleRoleChange(memberId, role) {
+        axios
+            .put(`/groups/${props.groupId}/members/${memberId}`, {role})
+            .then(function (response) {
+                message.success("修改成功");
+            })
+            .catch(function (error) {
+                message.error("修改失败");
+            });
+    }
+
     return (
         <div>
             {content.map((item) => (
                 <div className="group-member-management-item">
                     <div className="group-member-management-item-name">{item.name}</div>
                     <div>加入时间：{item.createdAt}</div>
-                    <div className="group-member-management-item-role">{roleMap[item.role]}</div>
+                    <Select defaultValue={item.role}
+                            style={{width: 120}}
+                            onChange={(value) => handleRoleChange(item.id, value)}
+                    >
+                        <Option value="NORMAL">成员</Option>
+                        <Option value="ADMIN">管理员</Option>
+                    </Select>
                     <Button type="primary" danger>移除</Button>
                 </div>
             ))}
