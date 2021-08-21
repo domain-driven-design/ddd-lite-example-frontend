4eac8e3445179512ddf0a3bb6b59a8a791444d20
hang <isixline@qq.com>
2021-08-21 22:56:17 +0800

    feat: disabled group member management for group admin

--- src/components/group/GroupManagement.js
+++ src/components/group/GroupManagement.js
@@ -1,13 +1,35 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 
 import "./GroupManagement.css";
-import {Menu} from "antd";
+import {Menu, message} from "antd";
 import GroupMemberManagement from "./GroupMemberManagement";
 import QuestionManagement from "../question/QuestionManagement";
+import axios from "../../common/axios";
 
 export default function GroupManagement(props) {
-    const [currentMenuKey, setCurrentMenuKey] = useState("member");
+    const [groupInfo, setGroupInfo] = useState({});
+    const [currentMember, setCurrentMember] = useState({});
+    const [currentMenuKey, setCurrentMenuKey] = useState();
+    const userId = window.localStorage.userId;
     const id = props.match.params.id;
+
+    useEffect(() => {
+        getGroup(id);
+    }, []);
+
+
+    function getGroup(id) {
+        axios.get(`/groups/${id}`)
+            .then(function (data) {
+                setGroupInfo(data);
+                const member = data.members?.find(member => member.userId === userId, {});
+                setCurrentMember(member);
+                setCurrentMenuKey(member.role === "OWNER" ? "member" : "question")
+            })
+            .catch(function (error) {
+                message.error("获取圈子详情失败");
+            });
+    }
 
     function handleMenuClick(e) {
         setCurrentMenuKey(e.key)
@@ -27,24 +49,27 @@ export default function GroupManagement(props) {
     }
 
     return (
-        <div className="group-management">
-            <Menu className="group-management-menu" onClick={handleMenuClick} selectedKeys={[currentMenuKey]}
-                  mode="vertical">
-                <Menu.Item key="member">
-                    成员管理
-                </Menu.Item>
-                <Menu.Item key="question">
-                    问题管理
-                </Menu.Item>
-                <Menu.Item key="answer">
-                    回答管理
-                </Menu.Item>
-                <Menu.Item key="setting">
-                    圈子设置
-                </Menu.Item>
-            </Menu>
-            <div className="group-management-content">
-                {showManagementContent(currentMenuKey)}
+        <div>
+            <a href={`/groups/${id}`}><h2>{groupInfo.name}</h2></a>
+            <div className="group-management">
+                <Menu className="group-management-menu" onClick={handleMenuClick} selectedKeys={[currentMenuKey]}
+                      mode="vertical">
+                    <Menu.Item disabled={currentMember.role !== "OWNER"} key="member">
+                        成员管理
+                    </Menu.Item>
+                    <Menu.Item key="question">
+                        问题管理
+                    </Menu.Item>
+                    <Menu.Item key="answer">
+                        回答管理
+                    </Menu.Item>
+                    <Menu.Item disabled={currentMember.role !== "OWNER"} key="setting">
+                        圈子设置
+                    </Menu.Item>
+                </Menu>
+                <div className="group-management-content">
+                    {showManagementContent(currentMenuKey)}
+                </div>
             </div>
         </div>
     );
