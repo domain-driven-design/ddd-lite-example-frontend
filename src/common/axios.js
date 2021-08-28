b44ca6daa0194268d654aea6e5663a2ce288a1a0
hang <isixline@qq.com>
2021-08-28 13:30:51 +0800

    fix: axios response interceptors

--- src/common/axios.js
+++ src/common/axios.js
@@ -1,36 +1,37 @@
 import axios from "axios";
-import { message } from "antd";
+import {message} from "antd";
 
 const service = axios.create({
-  timeout: 60000,
+    timeout: 60000,
 });
 
 service.interceptors.request.use(
-  (config) => {
-    if (window.localStorage.getItem("token")) {
-      config.headers["Authorization"] = `Bearer ${window.localStorage.getItem(
-        "token"
-      )}`;
+    (config) => {
+        if (window.localStorage.getItem("token")) {
+            config.headers["Authorization"] = `Bearer ${window.localStorage.getItem(
+                "token"
+            )}`;
+        }
+        return config;
+    },
+    (error) => {
+        Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    Promise.reject(error);
-  }
 );
 
+
 service.interceptors.response.use(
-  (response) => {
-    if (response.status === 401) {
-      message.error("认证失败，请重新登录");
-      return Promise.reject("error");
-    } else {
-      return response.data;
+    (response) => {
+        return response.data;
+    },
+    (error) => {
+        if (error.response.status === 401) {
+            message.error("认证失败，请重新登录");
+        } else {
+            message.error(error.response.data.message)
+        }
+        return Promise.reject(error);
     }
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
 
 export default service;
